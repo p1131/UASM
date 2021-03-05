f8cd5017d34e109868702fd4b46915462de531d0
Panos St <panos@rabbit.p1131.xyz>
2021-03-05 12:06:44 +0200

    Fix Linux build, tested on:   Linux user 5.10.0-3-amd64 #1 SMP Debian 5.10.13-1 (2021-02-06) x86_64 GNU/Linux   gcc (Debian 10.2.1-6) 10.2.1 20210110

---
+++ globals.c
@@ -0,0 +1,7 @@
+#include "globals.h"
+unsigned char decoflags = 0u;
+unsigned char broadflags = 0u;
+unsigned char evex = 0u;
+unsigned char evexflag = 0u;
+unsigned char ZEROLOCALS = 0u;
+unsigned char MODULEARCH = 0u;
